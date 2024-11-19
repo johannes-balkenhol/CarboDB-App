10fb8d4c39b7d8280a5093faf27ba408bb065364
Eva Rühl <eruehl@ai-ag.de>
2024-11-19 09:45:15 +0100

    Server: Minimal Flask app

---
+++ backend/main.py
@@ -0,0 +1,32 @@
+"""Main module for handling web server operations.
+
+This module sets up a Flask application to handle various web requests, including file uploads and data processing.
+CORS is enabled for all origins to allow cross-origin requests.
+
+"""
+
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+
+def create_app():
+    """
+        necessary for tests.
+    """
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    CORS(app, resources={r"/*": {"origins": "*"}})
+    return app
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+if __name__ == '__main__':
+    create_app()
+    app.run(debug=True)
