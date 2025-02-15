23772cd974f811d3184e474fa8726aaeeaae0c47
Eva <ruehl.eva@web.de>
2025-02-15 10:08:59 +0100

    server: added parsing to prosite_scan_use_case

--- backend/carboxylase_search/prosite_scan/prosite_scan_use_case.py
+++ backend/carboxylase_search/prosite_scan/prosite_scan_use_case.py
@@ -1,20 +1,78 @@
-from backend.carboxylase_search.prosite_scan.prosite_scan_utils import run_ps_scan_wit_custom_profile
+import re
+from backend.carboxylase_search.prosite_scan.prosite_scan_utils import run_ps_scan_with_custom_profile
+from backend.domain.PrositeSearchResult import PrositeSearchResult
 from backend.repository.PrositePatternRepository import PrositePatternRepository
 
 base_dir_ = "/home/eva/PycharmProjects/Carboxylase_Server/"
 seq_file_location_ = "/home/eva/PycharmProjects/Carboxylase_Server/backend/carboxylase_search/data_acquisition/out/PF00016_hits.fasta"
+ps_scan_output_directory = "backend/carboxylase_search/prosite_scan/output"
 
 
-def run_prosite_scan_workflow_for_all_patterns(repository, base_dir, seq_file_location):
+def run_prosite_scan_workflow_for_all_patterns(repository, base_dir, seq_file_location, ps_scan_output_directory):
+    """
+
+    Args:
+        repository: PrositePatternRepository that return the prosite patterns to screen for
+        base_dir: base directory where the project resides as determined dynamically in main.py
+        seq_file_location: location of the file containing the sequences
+
+    Returns:
+        The compiled search results in a dictionary with the prosite accession numbers as keys and lists of PrositeSearchResult objects as values
+
+    """
+
+    compiled_prosite_scan_results = {}
     patterns = repository.get_all_patterns()
     for pattern in patterns:
-        output_file_name = pattern
-        run_ps_scan_wit_custom_profile(base_dir, seq_file_location, pattern, output_file_name)
+        run_ps_scan_with_custom_profile(base_dir, seq_file_location, pattern, ps_scan_output_directory, pattern)
+        current_search_result = parse_prosite_scan_output(base_dir + ps_scan_output_directory + "/" + pattern)
+        compiled_prosite_scan_results.update({pattern: current_search_result})
+
+    return compiled_prosite_scan_results
 
 
 
 
+def parse_prosite_scan_output(output_file_location):
+    """
+
+    Args:
+        output_file_location: The file location where the prosite output resides
+
+    Returns:
+        A list of PrositeSearchResult object or an empty list
+    """
+
+    try:
+        with open(output_file_location, "r") as file:
+            lines = file.readlines()
+        results = [(lines[i].strip(), lines[i + 1].strip()) for i in range(0, len(lines), 2)]
+        prosite_search_results = []
+        for result in results:
+
+            #parse first part
+            parts1 = result[0].split(":")
+            sequence_id = parts1[0].strip()[1:]
+            parts2 = parts1[1].strip().split(" ", 1)
+            prosite_accession = parts2[0].strip()
+            prosite_name = parts2[1].strip()
+
+            #parse second part
+            parts3 = re.split(r'\s{2}', result[1])
+            parts4 = re.split(r'\s-\s', parts3[0])
+            start_position = parts4[0]
+            end_position = parts4[1]
+            pattern_sequence = parts3[1].strip()
+
+            current_search_result = PrositeSearchResult(sequence_id, prosite_accession, prosite_name, start_position, end_position, pattern_sequence)
+            prosite_search_results.append(current_search_result)
+
+        print(f"Contents successfully parsed from {output_file_location}")
+        return prosite_search_results
+    except Exception as e:
+        print(f"An error occurred while parsing from file {output_file_location}: {e}")
+        return []
 
 
 repository = PrositePatternRepository()
-run_prosite_scan_workflow_for_all_patterns(repository, base_dir_, seq_file_location_)
+run_prosite_scan_workflow_for_all_patterns(repository, base_dir_,seq_file_location_,ps_scan_output_directory)
--- backend/carboxylase_search/prosite_scan/prosite_scan_utils.py
+++ backend/carboxylase_search/prosite_scan/prosite_scan_utils.py
@@ -1,10 +1,10 @@
 import subprocess
 
 ps_scan_file_location = "resources/carboxylases/prosite/ps_scan/ps_scan.pl"
-ps_database_file_location = "resources/carboxylases/prosite/ps_scan/prosite.dat"
-ps_output_directory = "backend/carboxylase_search/prosite_scan/output"
+ps_scan_database_file_location = "resources/carboxylases/prosite/ps_scan/prosite.dat"
 
-def run_ps_scan_wit_custom_profile(base_dir, seq_file_location, pattern, output_file_name):
+
+def run_ps_scan_with_custom_profile(base_dir, seq_file_location, pattern, ps_scan_output_directory, output_file_name):
     """
         Function runs prosite scan (ps_scan)
 
@@ -12,14 +12,16 @@ def run_ps_scan_wit_custom_profile(base_dir, seq_file_location, pattern, output_
             base_dir: base directory of the project as defined in main.py
             seq_file_location: The FASTA file with sequences to use.
             pattern: the prosite accession number of the prosite motif to look for
+            ps_scan_output_directory: path to output directory in the project
             output_file_name: name of the file that is written as an output
 
         Returns:
             writes a file to the output directory
         """
     ps_scan_file_location_absolute_path = base_dir + ps_scan_file_location
-    ps_database_file_location_absolute_path = base_dir + ps_database_file_location
-    ps_output_directory_absolute_path = base_dir + ps_output_directory
+    ps_database_file_location_absolute_path = base_dir + ps_scan_database_file_location
+    ps_output_directory_absolute_path = base_dir + ps_scan_output_directory
+    print(ps_output_directory_absolute_path)
 
     cmd = ("perl " + ps_scan_file_location_absolute_path + " -d " + ps_database_file_location_absolute_path + " -p "+ pattern + " "
            + seq_file_location + " > " + ps_output_directory_absolute_path + "/" + output_file_name)
