0e4d134e5bd5e5b95fa4c743ab589f70425621eb
Eva <ruehl.eva@web.de>
2025-04-13 15:34:30 +0200

    server: tests for repos

--- backend/repository/PrositePatternRepository.py
+++ backend/repository/PrositePatternRepository.py
@@ -2,5 +2,6 @@
 
 class PrositePatternRepository:
     def get_all_patterns(self):
-        patterns = ["PS00157"]
+        patterns = ["PS00157", "PS00155", "PS00866", "PS00867", "PS00188", "PS00781", "PS00393", "PS00012", "PS00703", "PS00768", "PS00878",
+                    "PS00879", "PS00103", "PS00156", "PS01330", "PS01336", "PS00906", "PS00907", "PS00392", "PS00923", "PS00924"]
         return patterns
